719a04df543fd3f045b69ca948017c5a423d2e53
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-28 11:09:05 -0300

    banco local criado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,24 @@ import { UsersModule } from '../users/users.module';
 import { ClientsModule } from '../clients/clients.module';
 import { DemandsModule } from '../demands/demands.module';
 import { AuthModule } from '../auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [UsersModule, ClientsModule, DemandsModule, AuthModule],
+  imports: [ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: Number(process.env.DATABASE_PORT),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASS,
+      database: process.env.DATABASE_NAME,
+      autoLoadEntities: true,
+      synchronize: true, // apenas para desenvolvimento
+    }),
+    UsersModule, ClientsModule, DemandsModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
