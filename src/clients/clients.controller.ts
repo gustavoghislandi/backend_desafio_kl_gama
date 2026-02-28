53d55b3e126f1dac396825136d835c7cd423560d
gustavoghislandi <gugasouza3003@gmail.com>
2026-02-28 11:43:26 -0300

    Criado o POST para /clients

--- src/clients/clients.controller.ts
+++ src/clients/clients.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ClientsService } from './clients.service';
 
 @Controller('clients')
-export class ClientsController {}
+export class ClientsController {
+    constructor(
+        private readonly clientsService: ClientsService) {}
+    
+    @Post()
+    async create(@Body() body: { nome: string }){
+        return this.clientsService.create(body.nome)
+    }
+
+}
--- src/clients/clients.service.ts
+++ src/clients/clients.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { ClientEntity } from './entities/client.entity';
+import { Repository } from 'typeorm';
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+    constructor(
+        @InjectRepository(ClientEntity)
+        private readonly clientRepository: Repository<ClientEntity>,
+    ){}
+
+    async create(nome: string) {
+        const client = this.clientRepository.create({ nome });
+        return await this.clientRepository.save(client);
+    }
+}
